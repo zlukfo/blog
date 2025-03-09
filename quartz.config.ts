9479a8a7acbfea48c63935c0c9d70a96de4cf2ab
zlukfo <zlukfo@gmail.com>
2025-03-09 09:56:43 +0300

    Quartz sync: Mar 9, 2025, 9:56 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "zlukfo.github.io/blog",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: true,
